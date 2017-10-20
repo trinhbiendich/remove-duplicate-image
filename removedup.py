90558ccf9dfeb67e436b67fb8872773ae2415d01
Thanh Doan <thanh.doan@sutrixsolutions.com>
2017-10-20 14:41:55 +0700

    Commit new code to find and move all file

--- removedup.py
+++ removedup.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python
+#python removedup.py -i pics -o imgs
 
 import sys
 from PIL import Image
 import argparse
 import os
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, abspath
 import imghdr
 import uuid
+import shutil
+
+class FileItem:
+    def __init__(self):
+        self.path = ""
+        self.name = ""
+        self.ext = ""
+        self.h = ""
+        self.width = 0
+        self.height = 0
+    def __init__(self, name, ext, path, hash1, width, height):
+        self.path = path
+        self.name = name
+        self.ext = ext
+        self.h = hash1
+        self.width = width
+        self.height = height
+    def __repr__(self):
+        return " \n\n======== FileItem with filename:%s ========\npath:%s \nhash:%d \n%dx%d" % (self.name, 
+                self.path, self.h, self.width, self.height)
 
 def avhash(im):
     if not isinstance(im, Image.Image):
@@ -24,24 +45,18 @@ def hamming(h1, h2):
         h += 1
         d &= d - 1
     return h
-def tinhToanHamming(img1, img2):
-    hash1 = avhash(img1)
-    hash2 = avhash(img2)
-    dist = hamming(hash1, hash2)
+def tinhToanHamming(item1, item2):
+    dist = hamming(item1.h, item2.h)
     return dist
-def tinhToanPercent(img1, img2):
-    dist = tinhToanHamming(img1, img2)
+def tinhToanPercent(item1, item2):
+    dist = tinhToanHamming(item1, item2)
     percent = (64 - dist) * 100 / 64
     return percent
-def layAnhNhoNhat(img1, img2):
-    im1 = Image.open(img1)
-    im2 = Image.open(img2)
-    width1, height1 = im1.size
-    width2, height2 = im2.size
-    if width1 < width2:
-        return img1
+def layItemCoKichThuocNhoHon(item1, item2):
+    if item1.width < item2.width:
+        return item1
     else:
-        return img2
+        return item2
 
 if __name__ == '__main__':
     # construct the argument parse and parse the arguments
@@ -50,36 +65,68 @@ if __name__ == '__main__':
         help = "path to input images")
     ap.add_argument("-o", "--output", required = True,
         help = "output folder")
-    ap.add_argument("-max", "--percentsame", required = False, default=90,
-        help = "Max percent same value default 90%")
+    ap.add_argument("-max", "--percentsame", required = False, default=95,
+        help = "Max percent same value default 95%")
     args = vars(ap.parse_args())
 
-    inputFolder = os.path.join(args["input"], '')
-    outputFolder = os.path.join(args["output"], '')
+    #Define variable
+    inputFolder = abspath(os.path.join(args["input"], ''))
+    outputFolder = abspath(os.path.join(args["output"], ''))
+    notImgFolder = join(outputFolder, "not_img")
     maxPercent = int(float(args["percentsame"]))
+    objs = []
+
+    #create folder if it not exists
     if not os.path.exists(outputFolder):
         os.makedirs(outputFolder)
+    if not os.path.exists(notImgFolder):
+        os.makedirs(notImgFolder)
+
+    #Print for view
     print "Input folder : ", inputFolder
     print "Output folder : ", outputFolder
-    onlyfiles = []
-    for f in listdir(inputFolder):
-        fullPath = join(inputFolder, f)
-        if isfile(join(inputFolder, f)) and imghdr.what(fullPath) != None:
-            onlyfiles.append(f)
-    for f in onlyfiles:
-        fullPath = join(inputFolder, f)
-        for f2 in onlyfiles:
-            fullPath2 = join(inputFolder, f2)
-            if os.path.exists(fullPath2) and os.path.exists(fullPath) and f != f2:
-                percent = tinhToanPercent(fullPath, fullPath2)
-                if(percent >= maxPercent):
-                    #move to new folder
-                    movePath = layAnhNhoNhat(fullPath, fullPath2)
-                    filename, file_extension = os.path.splitext(movePath)
+
+    valid_images = [".jpg",".png", ".jpeg"]
+    for path, subdirs, files in os.walk(inputFolder):
+        for f in files:
+            fullPath = join(path, f)
+            ext = os.path.splitext(f)[1]
+            if ext.lower() not in valid_images:
+                notImgPath = join(notImgFolder, f)
+                if os.path.exists(notImgPath):
                     tmpName = str(uuid.uuid4())
-                    newName = tmpName + file_extension
+                    newName = tmpName + ext
+                    notImgPath = join(notImgFolder, newName)
+                shutil.move(fullPath, notImgPath)
+                continue
+            if imghdr.what(fullPath) != None:
+                img = Image.open(fullPath)
+                width, height = img.size
+                hash1 = avhash(img)
+                obj = FileItem(f, ext, fullPath, hash1, width, height)
+                objs.append(obj)
+            else:
+                notImgPath = join(notImgFolder, f)
+                if os.path.exists(notImgPath):
+                    tmpName = str(uuid.uuid4())
+                    newName = tmpName + ext
+                    notImgPath = join(notImgFolder, newName)
+                shutil.move(fullPath, notImgPath)
+
+    for item1 in objs:
+        for item2 in objs:
+            if not os.path.exists(item1.path) or not os.path.exists(item2.path) or item1.path == item2.path:
+                continue
+            percent = tinhToanPercent(item1, item2)
+            if(percent >= maxPercent):
+                #move to new folder
+                item = layItemCoKichThuocNhoHon(item1, item2)
+                tmpName = str(uuid.uuid4())
+                newPath = join(outputFolder, item.name)
+                if os.path.exists(newPath):
+                    newName = tmpName + item.ext
                     newPath = join(outputFolder, newName)
-                    print "percent ", percent
-                    os.rename(movePath, newPath)
+                print "percent ", percent, " - ", item.name
+                shutil.move(item.path, newPath)
     print "Done!!!"
 
